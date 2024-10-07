6c7863e3e1170c57a6042c86d9ce389c680e884b
Alex <alexander.tastad@hotmail.com>
2024-10-07 16:59:53 +0200

    Add: views for booking list and appointment

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookingForm
+from .models import Booking
 
-# Create your views here.
+def booking_list(request):
+    bookings = Booking.objects.all()
+    return render(request, 'bookings/booking_list.html', {'bookings': bookings})
+
+def book_appointment(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_list')
+    else:
+        form = BookingForm()
+    return render(request, 'bookings/book_appointment.html', {'form': form})
