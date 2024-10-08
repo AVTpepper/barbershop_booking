8dc16a083a50f322bda4af2049f7499d1e71f142
Alex <alexander.tastad@hotmail.com>
2024-10-08 12:51:02 +0200

    Add: View and URL for booking calendar

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.booking_list, name='booking_list'),
     path('book/', views.book_appointment, name='book_appointment'),
+    path('calendar/', views.booking_calendar, name='booking_calendar'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,6 @@
+import json
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .forms import BookingForm
 from .models import Booking
 
@@ -15,3 +17,13 @@ def book_appointment(request):
     else:
         form = BookingForm()
     return render(request, 'bookings/book_appointment.html', {'form': form})
+
+def booking_calendar(request):
+    bookings = Booking.objects.all()
+    events = [
+        {
+            'title': f"{booking.customer_name} - {booking.service.name}",
+            'start': f"{booking.date}T{booking.time}",
+        } for booking in bookings
+    ]
+    return render(request, 'bookings/booking_calendar.html', {'events': json.dumps(events)})
