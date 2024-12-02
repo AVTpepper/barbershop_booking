ea3f60d0635e2860f80b19b017fdc2cb8341132e
Alex <alexander.tastad@hotmail.com>
2024-12-02 14:54:04 +0100

    Add: URL for booking success page

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.booking_list, name='booking_list'),
     path('book/', views.book_appointment, name='book_appointment'),
+    path('success/', views.booking_success, name='booking_success'),
     path('calendar/', views.booking_calendar, name='booking_calendar'),
 ]
