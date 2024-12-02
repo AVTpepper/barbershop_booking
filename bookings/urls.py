5d2e0de0848186112e372e22c6fc9b42a9a154d1
Alex <alexander.tastad@hotmail.com>
2024-12-02 16:14:43 +0100

    Add: URL for deleting an appointment

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.booking_list, name='booking_list'),
     path('book/', views.book_appointment, name='book_appointment'),
     path('update/<int:booking_id>/', views.update_appointment, name='update_appointment'),
+    path('delete/<int:booking_id>/', views.delete_appointment, name='delete_appointment'),
     path('success/', views.booking_success, name='booking_success'),
     path('calendar/', views.booking_calendar, name='booking_calendar'),
 ]
