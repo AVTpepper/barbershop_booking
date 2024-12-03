f75b139c2b35bf32170002b50c31d776b7ba33ae
Alex <alexander.tastad@hotmail.com>
2024-12-03 15:27:36 +0100

    Update: allowed host for render

--- barbershop_booking/settings.py
+++ barbershop_booking/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-2qmnq+jlwib#mc7)u-$w@9vtbc_sw@zxt7lhvj4mibb8gaw%4b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://avt-barbershop-booking-4badacb40fc3.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://barbershop-booking.onrender.com', '127.0.0.1']
 
 
 # Application definition
