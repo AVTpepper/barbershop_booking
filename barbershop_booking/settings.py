46498e77107d232a0ab14a52507553abdb716b5a
Alex <alexander.tastad@hotmail.com>
2024-12-03 01:54:19 +0100

    Update: allowed hosts

--- barbershop_booking/settings.py
+++ barbershop_booking/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-2qmnq+jlwib#mc7)u-$w@9vtbc_sw@zxt7lhvj4mibb8gaw%4b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-heroku-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://avt-barbershop-booking-4badacb40fc3.herokuapp.com/', 'localhost']
 
 
 # Application definition
