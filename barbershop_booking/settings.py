5ebc64eb7aaf849718f17d199adbc158e1a0ba28
Alex <alexander.tastad@hotmail.com>
2024-12-03 01:13:18 +0100

    Update: Add Heroku to ALLOWED_HOSTS

--- barbershop_booking/settings.py
+++ barbershop_booking/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-2qmnq+jlwib#mc7)u-$w@9vtbc_sw@zxt7lhvj4mibb8gaw%4b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-heroku-app.herokuapp.com', 'localhost']
 
 
 # Application definition
