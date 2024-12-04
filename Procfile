f03f3c338411d028d8ce32a44b801a98af3e90a0
Alex <alexander.tastad@hotmail.com>
2024-12-04 14:49:35 +0100

    Update: remove heroku connections, database and requirements added, admin panel options

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn barbershop_booking.wsgi
--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Barber, Service, Booking
 
 # Register your models here.
+admin.site.register(Barber)
+admin.site.register(Service)
+admin.site.register(Booking)
